59b6e090fa09587828cef27fb0484fabf2eb3ed2
Mike DePaulo <mikedep333@redhat.com>
2019-08-13 11:33:55 -0400

    WIP 14.1 on using containers for CI
    [noissue]

--- .travis/func_test_script.sh
+++ .travis/func_test_script.sh
@@ -3,6 +3,6 @@
 
 set -mveuo pipefail
 
-$CMD_PREFIX pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional || show_logs_and_return_non_zero
-$CMD_PREFIX pytest -v -r sx --color=yes --pyargs pulp_file.tests.functional || show_logs_and_return_non_zero
-$CMD_PREFIX pytest -v -r sx --color=yes --pyargs pulp_certguard.tests.functional || show_logs_and_return_non_zero
+pytest -v -r sx --color=yes --pyargs pulpcore.tests.functional || show_logs_and_return_non_zero
+pytest -v -r sx --color=yes --pyargs pulp_file.tests.functional || show_logs_and_return_non_zero
+pytest -v -r sx --color=yes --pyargs pulp_certguard.tests.functional || show_logs_and_return_non_zero
